b26cd0d62379cb26b84ea88e0e3e8c1846f4d093
Eranda Bandaranaike <ergroove@live.com>
2025-03-05 12:59:28 +0530

    Doctor selection issue has been sorted out

--- app/page.tsx
+++ app/page.tsx
@@ -133,7 +133,7 @@ export default function Home() {
                 </div>
             </section>
             {/* Services Section */}
-            <section className="bg-gradient-to-br from-amber-50 via-red-50 to-violet-50 lg:py-24">
+            <section className="bg-gradient-to-br from-amber-50 via-red-50 to-violet-50 lg:py-24 py-6">
                 <div className="max-w-7xl mx-auto p-4 lg:p-0">
                     <div className="text-center pb-12">
                         <h2 className="text-4xl font-bold">Our Comprehensive Healthcare Services</h2>
