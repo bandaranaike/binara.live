ad2a3c55cdc144cf5367a6f948833029f993dae1
Eranda Bandaranaike <eranda.b@antlabs.com>
2025-12-11 17:08:35 +0530

    Updated pipeline and ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     apps: [
         {
-            name: 'binara.live',
-            script: 'npx',
-            args: 'next start',
+            name: "binara.live",
+            cwd: "/var/www/binara.live",
+            script: "node_modules/.bin/next",
+            args: "start -p 3000",
             env: {
-                NODE_ENV: 'production',
-                PORT: 3004,
-            },
-        },
-    ],
+                NODE_ENV: "production"
+            }
+        }
+    ]
 };
